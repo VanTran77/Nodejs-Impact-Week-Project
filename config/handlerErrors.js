a6cd17a754db777c8c49472b7630b251ab6fda00
Van <tavan.it@gmail.com>
2022-01-30 14:11:36 +0100

    update screen (homepage, login, signup) & questiion cards

--- config/handlerErrors.js
+++ config/handlerErrors.js
@@ -1,7 +1,7 @@
 const handlerError = (err) => {
 const errs = {}
 Object.values(err.errors).forEach((data) => {
-      console.log(data.properties);
+      // console.log(data.properties);
       errs[data.properties.path] = data.properties.message
       })
       //  console.log(err.errors);
